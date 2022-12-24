bf490f8af546a31221756a9ea21fd73826f90ac5
lullullalra <pjh3668@gmail.com>
2022-12-24 10:32:33 +0900

    flutter를 사용한 퀴즈앱

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice/screen/screen_home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Quiz App',
+      home: HomeScreen(),
+    );
+  }
+}
+
